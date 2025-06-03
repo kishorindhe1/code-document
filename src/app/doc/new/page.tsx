d25d8485b2d6c8f8936a782e26f991e81c44a5b2
kishor <kishori@winjit.com>
2025-06-03 21:45:33 +0530

    refactor: revert EditDocument import to static loading for consistency

--- src/app/doc/new/page.tsx
+++ src/app/doc/new/page.tsx
@@ -1,10 +1,7 @@
+'use client';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
 import Link from 'next/link';
-import dynamic from 'next/dynamic';
-
-const EditDocument = dynamic(() => import('@/components/views/EditDocument'), {
-    ssr: false, // Disables server-side rendering
-});
+import EditDocument from '@/components/views/EditDocument';
 
 const page = () => {
     return (
